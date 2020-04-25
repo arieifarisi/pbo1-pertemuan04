df24fc0c80b715ac695cf8d282cb40cc0f60974e
ariefarisi <mif160399@gmail.com>
2020-04-25 17:39:32 +0800

    array

---
+++ src/array/Array3.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Array3 {
+    public static void main(String[] args) {
+
+        int ukuranArr = 10;
+        int hasil = 0;
+        int[] bilangan = new int[ukuranArr];
+
+        for (int i = 0; i < ukuranArr; i++) {
+            bilangan[i] = (i + 1) * 10;
+        }
+        for (int i = 0; i < ukuranArr; i++) {
+            System.out.println("bilangan[" + i + "] =  " + bilangan[i]);
+            hasil += bilangan[i];
+        }
+
+        System.out.println("Hasil penjumlahan =  " + hasil);
+
+    }
+}
+
