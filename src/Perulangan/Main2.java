2e308b220e5f0202d546070a4cd1c905539eb2d9
ariefarisi <mif160399@gmail.com>
2020-04-25 17:20:30 +0800

    saat isiGelas di ganti jadi 255, maka akan terjadi infinite loop

--- src/Perulangan/Main2.java
+++ src/Perulangan/Main2.java
@@ -1,8 +1,8 @@
 package Perulangan;
-
+//
 public class Main2 {
     public static void main(String[] args) {
-        int isiGelas = 0;
+        int isiGelas = 225;
         int penuh = 225;
 
         System.out.println("Isi Gelas Sekarang :" + isiGelas + "ml");
